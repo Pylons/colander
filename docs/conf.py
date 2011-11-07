164d7dec0953e44681bfd24e700ddbad6dab2dfa
Blaise Laflamme <blaise@laflamme.org>
2011-11-07 15:44:42 -0800

    updated docs for rtd

--- docs/conf.py
+++ docs/conf.py
@@ -93,7 +93,8 @@ pygments_style = 'sphinx'
 
 sys.path.append(os.path.abspath('_themes'))
 html_theme_path = ['_themes']
-html_theme = 'pylons'
+html_theme = 'pyramid'
+html_theme_options = dict(github_url='https://github.com/Pylons/colander')
 
 # The style sheet to use for HTML and HTML Help pages. A file of that name
 # must exist either in Sphinx' static/ path, or in one of the custom paths
