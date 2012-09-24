f8551dde289d39ceabf60c0eb8dbab17bb847b75
Chris McDonough <chrism@plope.com>
2012-09-24 16:20:26 -0400

    prep for 0.9.9

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ testing_extras = ['nose', 'coverage']
 docs_extras = ['Sphinx']
 
 setup(name='colander',
-      version='0.9.9dev',
+      version='0.9.9',
       description=('A simple schema-based serialization and deserialization '
                    'library'),
       long_description=README + '\n\n' +  CHANGES,
