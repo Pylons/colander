e2ef91c5cb4c4e9ff27531450d3d628fe54af6b4
Chris McDonough <chrism@plope.com>
2012-10-09 23:34:21 -0400

    vb

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ testing_extras = ['nose', 'coverage']
 docs_extras = ['Sphinx']
 
 setup(name='colander',
-      version='0.9.9',
+      version='0.9.9.1dev',
       description=('A simple schema-based serialization and deserialization '
                    'library'),
       long_description=README + '\n\n' +  CHANGES,
