a258b087aeb45d270463fc080380cc7e67b4fe42
Michael Merickel <michael@merickel.org>
2016-01-18 19:46:28 -0600

    prep 1.2

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ docs_extras = [
 ]
 
 setup(name='colander',
-      version='1.1',
+      version='1.2',
       description=('A simple schema-based serialization and deserialization '
                    'library'),
       long_description=README + '\n\n' +  CHANGES,
