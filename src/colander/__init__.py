353838530af74e742034bd8522b75ad925cfd03c
Gouji Ochiai <gjo.ext@gmail.com>
2022-11-03 14:03:25 +0900

    refactor: format

--- src/colander/__init__.py
+++ src/colander/__init__.py
@@ -623,13 +623,11 @@ def _make_url_regex_src():
 
     # Host patterns
     hostname_re = (
-        r"[a-z"
-        + ul
-        + r"0-9](?:[a-z"
-        + ul
-        + r"0-9-]{0,61}[a-z"
-        + ul
-        + r"0-9])?"
+        (r"[a-z" + ul + r"0-9]")
+        + r"(?:"
+        + (r"[a-z" + ul + r"0-9-]{0,61}")
+        + (r"[a-z" + ul + r"0-9]")
+        + r")?"
     )
     # Max length for domain name labels is 63 characters per RFC 1034 sec. 3.1
     domain_re = r"(?:\.(?!-)[a-z" + ul + r"0-9-]{1,63}(?<!-))*"
