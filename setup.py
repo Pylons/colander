321407c5eea0f310b61b17b01482c7b70f3d7300
Michael Merickel <michael@merickel.org>
2023-01-02 19:52:54 -0600

    update localizations

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
+from babel.messages import frontend as babel
 from setuptools import setup
 
-setup()
+setup(
+    cmdclass={
+        'compile_catalog': babel.compile_catalog,
+        'extract_messages': babel.extract_messages,
+        'init_catalog': babel.init_catalog,
+        'update_catalog': babel.update_catalog,
+    }
+)
